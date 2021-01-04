b67c39ea9be1be86ba5a002e863dd4de0acbbe42
AskaTien <constantine.sky@hotmail.com>
2021-01-04 23:23:53 +0800

    Add project files.

---
+++ Engine/WinMain.cpp
@@ -0,0 +1,14 @@
+﻿#include "pch.h"
+#include "D3D12HelloWindow.h"
+
+using namespace winrt;
+namespace winrt
+{
+    using namespace Windows::Foundation;
+}
+
+int __stdcall wWinMain(HINSTANCE hInstance, HINSTANCE, LPWSTR, int nCmdShow)
+{
+    D3D12HelloWindow sample(1280, 720, L"D3D12 Hello Window");
+    return Win32Application::Run(&sample, hInstance, nCmdShow);
+}
---
+++ Engine/pch.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include <windows.h>
+#ifdef GetCurrentTime
+#undef GetCurrentTime
+#endif
+
+#include <d3d12.h>
+#include <dxgi1_6.h>
+//#include <D3Dcompiler.h>
+//#include <DirectXMath.h>
+#include "d3dx12.h"
+
+//#include <string>
+//#include <wrl.h>
+#include <shellapi.h>
+
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Windows.System.h>
+#include <winrt/Windows.UI.Xaml.h>
+#include <winrt/Windows.UI.Xaml.Controls.h>
+#include <winrt/Windows.UI.Xaml.Hosting.h>
+#include <winrt/Windows.UI.Xaml.Media.h>
+#include <Windows.UI.Xaml.Hosting.DesktopWindowXamlSource.h>
