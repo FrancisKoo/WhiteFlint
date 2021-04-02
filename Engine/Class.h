b11a01e78a801262017aa5fc5cedb9954adc5119
TienAska <constantine.sky@hotmail.com>
2021-04-02 19:25:38 +0800

    Add reunion projects

---
+++ Engine/Class.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "Class.g.h"
+
+namespace winrt::Engine::implementation
+{
+    struct Class : ClassT<Class>
+    {
+        Class() = default;
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+    public:
+        static const wchar_t* GetEngineName()
+        {
+            return L"WhiteFlint";
+        }
+    };
+}
+
+namespace winrt::Engine::factory_implementation
+{
+    struct Class : ClassT<Class, implementation::Class>
+    {
+    };
+}
